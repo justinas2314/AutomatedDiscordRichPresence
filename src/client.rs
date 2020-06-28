9bb76af54ad5635b55e3f4a339eac1f3fa7a0cc5
Justinas Staskevicius <staskevicius14@gmail.com>
2020-06-28 21:19:32 +0300

    committed the first version

---
+++ src/client.rs
@@ -0,0 +1,92 @@
+extern crate discord_rpc_client;
+
+
+use discord_rpc_client::Client;
+use std::collections::HashMap;
+
+
+fn clear(client: &mut Client) {
+	if let Err(e) = client.clear_activity() {
+		println!("error occurred while clearing the activity \n{}", e);
+	}
+}
+
+
+fn function(client: &mut Client, vector: &Vec<&str>,
+			raw_defaults: Option<&(&str, &str, &str, &str)>) {
+	let defaults = match raw_defaults {
+		Some(x) => *x,
+		None => ("", "", "", "") // causes an error later (that's intended lol)
+	};
+	let mut details = match vector.get(1) {
+		Some(x) if defaults.0 == ".." => *x,
+		Some(_) if defaults.0.len() < 4 => "    ",
+		Some(_) => defaults.0,
+		None if defaults.0.len() < 4 => "    ",
+		None => defaults.0
+	};
+	let mut state = match vector.get(2) {
+		Some(_) if defaults.1 == ".." && details.len() >= 20 => "", // will be replaced
+		Some(x) if defaults.1 == ".." => *x,
+		Some(_) if defaults.1.len() < 4 => "    ",
+		Some(_) => defaults.1,
+		None if defaults.1 == ".." && details.len() >= 20 => "", // will also be replaced
+		None if defaults.1.len() < 4 => "    ",
+		None => defaults.1
+	};
+	if state.len() == 0 && details.len() >= 20 {
+		let output = split_line(details);
+		details = output.0; // why couldn't I use deconstruction (tuple unpacking) here?
+		state = output.1; // the compiler decided to complain lol
+	}
+	// if let Err(e) = client.clear_activity() {
+	// 	println!("an error occured while clearing the activity \n {}", e);
+	// }
+	if let Err(e) = client.set_activity(|activity| activity
+		.details(details)
+		.state(state)
+		.assets(|asset| asset
+			.large_image(defaults.2)
+			.small_image(defaults.3))) {
+		println!("error occurred while setting an activity \n {}", e);
+		println!("details -> \t ,{}; ,{}; ,{}; ,{};", state, details, defaults.2, defaults.3);
+	}
+}
+
+
+fn split_line(line: &str) -> (&str, &str) {
+	let mut smaller_split_index = 0;
+	let mut bigger_split_index = 0;
+	for (i, j) in line.chars().enumerate() {
+		if i > 20 {
+			bigger_split_index = i;
+			break
+		}
+		if j == ' ' {
+			smaller_split_index = i;
+		}
+	}
+	let split_index = match (smaller_split_index, bigger_split_index) {
+		(x, y) if (20 - x as i32) * (20 - x as i32) > (20 - y as i32) * (20 - y as i32) => y,
+		(x, _) => x,
+	};
+	(&line[0..split_index], &line[split_index + 1..])
+}
+
+
+pub fn main(mut client: &mut Client, presets: &HashMap<&str, (&str, &str, &str, &str)>,
+				parsed_input: Vec<&str>) {
+	// ',,' seperates the values
+	// spaces between the values are stripped
+	// '..' skips the value and uses the default one
+	// ',, ,,' leaves the value as ''
+	// ',,,' splits and writes a comma (,)
+	// ',,,,' <- this is retarded and messes the code up don't do this
+	match parsed_input.get(0){
+		Some(&"clear") => clear(&mut client),
+		Some(x) => function(&mut client,
+							&parsed_input,
+							presets.get(*x)),
+		_ => ()
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod get_apps;
+mod read_commands;
+mod client;
+
+extern crate discord_rpc_client;
+
+use discord_rpc_client::Client;
+
+fn main() {
+    // doesn't work on linux yet
+    let contents= std::fs::read_to_string(
+        "C:\\Users\\stask\\IdeaProjects\\DiscordRichActivity\\src\\commands.txt").unwrap();
+    let mut rpc_client = Client::new(696035653711953981)
+        .unwrap();
+    rpc_client.start();
+    let commands = read_commands::main(&contents);
+    let order = read_commands::order(&contents);
+    loop {
+        // no sleeping because this part is really slow (thanks windows)
+        let running_apps = get_apps::main(&commands);
+        let running_app = match read_commands::get_ordered(running_apps, &order) {
+            Some(x) => x,
+            None => ("clear".to_string(), "clear".to_string())
+        };
+        println!("{}\n{}", &running_app.1, &running_app.0);
+        client::main(&mut rpc_client, &commands,
+                     vec![&running_app.1, &running_app.0]);
+    }
+}
